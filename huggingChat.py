1e26297a6911d577321754beeace0edca7bb418b
Harshil-Agrawal <94260691+Harshil-Agrawal@users.noreply.github.com>
2023-09-16 15:41:32 +0530

    Update huggingChat.py

--- huggingChat.py
+++ huggingChat.py
@@ -4,6 +4,8 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from dotenv import load_dotenv
+set HUGGINGFACEHUB_API_TOKEN=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
+
 
 import pandas as pd
 from model import PredictReview
